7c203555feece908e9ae56735d46d7e5ea33c1ba
MohnishKalia <mohnish.kalia@gmail.com>
2020-10-04 18:56:28 -0500

    Added comment

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+// Dont change this class, its just there for the folder
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
