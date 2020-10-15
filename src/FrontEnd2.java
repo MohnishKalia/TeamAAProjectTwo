6815f30641f79f91fc5175624e386453d4fc3406
MohnishKalia <mohnish.kalia@gmail.com>
2020-10-15 14:22:23 -0500

    Updates to comments, format, and error handling

--- src/FrontEnd2.java
+++ src/FrontEnd2.java
@@ -48,47 +48,10 @@ public class FrontEnd2 {
 
     }
 
-    /**
-     * Prints the PS2
-     * (https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#:~:text=PS2)
-     * when the user is prompted for input
-     */
-    private static void printInputPrefix() {
-        System.out.print("> ");
-    }
-
-    /**
-     * Gets the formatted error string for a given error message
-     * 
-     * @param message the error message you wish to format
-     * @return the message with a new line at the beginning and surrounded with
-     *         exclamation points
-     */
-    private static String getErrorString(String message) {
-        return String.format("\n! %s !", message);
-    }
-
     private static final String INVALID_INPUT_MESSAGE = "Invalid Input Field";
+    private static final String INVALID_WISC_ID_MESSAGE = "Provided WiscID was not a number";
+    private static final String INVALID_ENUM_CONSTANT_MESSAGE_SUFFIX = "is not a valid choice";
     private static final String WISC_ID_PROMPT = "Input WiscID:";
-
-    /**
-     * Runs a block of code while appropriately handling errors from both user input
-     * and state operations
-     * 
-     * @param codeBlock block of code to run under try-catch
-     */
-    private static void runWithCatch(Runnable codeBlock) {
-        try {
-            codeBlock.run();
-        } catch (IllegalArgumentException e) {
-            // user input was malformed
-            System.out.println(getErrorString(INVALID_INPUT_MESSAGE));
-        } catch (Exception e) {
-            // TODO: Add StateException here instead of generic exception
-            // user input was correctly formed, but is not valid
-            System.out.println(getErrorString(e.getMessage()));
-        }
-    }
 
     public static void main(String[] args) {
         // try-with-resources
@@ -97,7 +60,7 @@ public class FrontEnd2 {
             // loop is infinite because program should be canceled with the EXIT method
             // (^C and Stop button on IDE will terminate the program completely)
             while (true) {
-                runWithCatch(() -> {
+                runWithErrorHandling(() -> {
                     System.out.println("Please enter an operation:");
                     // print out the set of enum constants for the user
                     System.out.println(EnumSet.allOf(Operation.class));
@@ -113,7 +76,60 @@ public class FrontEnd2 {
         }
     }
 
-    // Enum Operation Implementations Below
+    /**
+     * Prints the PS2
+     * (https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#:~:text=PS2)
+     * when the user is prompted for input
+     */
+    private static void printInputPrefix() {
+        System.out.print("> ");
+    }
+
+    /**
+     * Gets the formatted error string for a given error message surrounded with a
+     * given string
+     * 
+     * @param surrounder the String that will surround the message
+     * @param message    the error message you wish to format
+     * @return the message with a new line at the beginning and surrounded with the
+     *         surrounder string
+     */
+    private static String getErrorString(String surrounder, String message) {
+        return String.format("\n%s %s %s", surrounder, message, surrounder);
+    }
+
+    /**
+     * Runs a block of code while appropriately handling errors from both user input
+     * and state operations
+     * 
+     * @param codeBlock block of code to run under try-catch
+     */
+    private static void runWithErrorHandling(Runnable codeBlock) {
+        try {
+            codeBlock.run();
+        } catch (NumberFormatException e) {
+            // error was from an invalid parse to long (NumberFormatException),
+            // so show a more accurate error message
+            System.out
+                    .println(getErrorString("!", INVALID_INPUT_MESSAGE) + getErrorString("~", INVALID_WISC_ID_MESSAGE));
+        } catch (IllegalArgumentException e) {
+            // user input was correctly formed but not valid, or is not valid enum choice
+
+            // storing in local variable for reuse
+            String errorMessage = e.getMessage();
+            // get the array of parts from the error message to extract the last element
+            // (user input) later if invalid enum choice
+            String[] parts = errorMessage.split("\\.");
+            // if the error was from an invalid enum constant choice,
+            // then show a more accurate error message
+            System.out.println(getErrorString("!", INVALID_INPUT_MESSAGE) + getErrorString("~",
+                    errorMessage.contains("enum constant")
+                            ? String.format("%s %s", parts[parts.length - 1], INVALID_ENUM_CONSTANT_MESSAGE_SUFFIX)
+                            : errorMessage));
+        }
+    }
+
+    // *** Enum Operation Implementations Below ***
 
     /**
      * The state instance that is used for the operations
@@ -121,7 +137,7 @@ public class FrontEnd2 {
     private static final State1 state = new State1();
 
     public static void add(Scanner input) {
-        runWithCatch(() -> {
+        runWithErrorHandling(() -> {
             System.out.println(WISC_ID_PROMPT);
             printInputPrefix();
             long wiscID = Long.parseLong(input.nextLine());
@@ -142,18 +158,17 @@ public class FrontEnd2 {
     }
 
     public static void remove(Scanner input) {
-        runWithCatch(() -> {
+        runWithErrorHandling(() -> {
             System.out.println(WISC_ID_PROMPT);
             printInputPrefix();
             long wiscID = Long.parseLong(input.nextLine());
 
-            state.removeMember(wiscID);
-            System.out.println("Member removed!");
+            System.out.println("Member removed!" + "\n" + state.removeMember(wiscID));
         });
     }
 
     public static void get(Scanner input) {
-        runWithCatch(() -> {
+        runWithErrorHandling(() -> {
             System.out.println(WISC_ID_PROMPT);
             printInputPrefix();
             long wiscID = Long.parseLong(input.nextLine());
