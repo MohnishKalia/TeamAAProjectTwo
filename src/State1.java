4e290158c4339b2ddedfc4c36158d58881dd7d5a
Grant Parfrey <gparfrey@wisc.edu>
2020-10-15 21:15:37 -0500

    Minor updates

--- src/State1.java
+++ src/State1.java
@@ -6,27 +6,51 @@
 // Lecturer: Florian Heimerl
 // Notes to Grader: n/a
 
+import java.util.EnumSet;
+import java.util.LinkedList;
+
 public class State1{
 
-    private RedBlackTree<Member> tree = DataUtils.loadData();
-    private LinkedList<long> keys;
+    private RedBlackTree<Member> tree;// = DataUtils.loadData();
+    private LinkedList<Long> keys;
 
 
-    private class Member{
-        public enum SchoolYear {
+    private static class Member implements Comparable<Member> {
+        enum SchoolYear {
             FRESHMAN, SOPHOMORE, JUNIOR, SENIOR, OTHER
         }
+
         public Member(long iwiscID, String iname, SchoolYear iyearInSchool){
             this.wiscID = iwiscID;
             this.name = iname;
             this.yearInSchool = iyearInSchool;
         }
+
+        public int compareTo(Member m){
+            return this.wiscID.compareTo(m.wiscID);
+        }
+
         //instance fields for Member:
         Long wiscID;
         String name;
         SchoolYear yearInSchool;
     }
 
+    //public Member getMember(long iwiscID){
+
+
+      //  return ;
+    //}
+
+    /*
+    TODO:
+        Member getMember(long wiscID)
+        Member removeMember(long wiscID)
+        void insertMember(long wiscID, String name, SchoolYear yearInSchool)
+        String toString()
+        save()
+
+     */
 
 
 
