e4c0a88832f646fdd60cdbcca794ae8d76fa9e5d
Grant Parfrey <gparfrey@wisc.edu>
2020-10-18 11:09:45 -0500

    Add, PrintAll, and Get are done, need data wrangler and comments

--- src/Member1.java
+++ src/Member1.java
@@ -1,12 +1,14 @@
+import java.time.LocalTime;
 public class Member1 implements Comparable<Member1> {
     enum SchoolYear {
         FRESHMAN, SOPHOMORE, JUNIOR, SENIOR, OTHER
     }
 
-    public Member1(long iwiscID, String iname, SchoolYear iyearInSchool){
+    public Member1(long iwiscID, String iname, SchoolYear iyearInSchool, LocalTime itime){
         this.wiscID = iwiscID;
         this.name = iname;
         this.yearInSchool = iyearInSchool;
+        this.time = itime;
     }
 
     public int compareTo(Member1 m){
@@ -17,6 +19,12 @@ public class Member1 implements Comparable<Member1> {
     Long wiscID;
     String name;
     SchoolYear yearInSchool;
+    LocalTime time;
+
+    @Override
+    public String toString(){
+        return (wiscID.toString()+","+name+","+yearInSchool+","+time);
+    }
 
 
 }
--- src/State1.java
+++ src/State1.java
@@ -6,7 +6,7 @@
 // Lecturer: Florian Heimerl
 // Notes to Grader: n/a
 
-import java.util.EnumSet;
+import java.time.LocalTime;
 import java.util.LinkedList;
 
 public class State1{
@@ -21,27 +21,80 @@ public class State1{
         tree = new RedBlackTree<Member1>();
     }
     public void insertMember(long wiscID, String name, Member1.SchoolYear yearInSchool) throws IllegalArgumentException{
-        tree.insert(new Member1(wiscID, name, yearInSchool));
+        //Makes sure that the wiscID is 9 digits long. First digit must not be zero
+        if(wiscID < Long.valueOf(1000000000*10) && wiscID > Long.valueOf(999999999)){
+            throw new IllegalArgumentException("WiscID was not 9 digits long or had a starting digit of 0.");
+        }
+        LocalTime time = LocalTime.now();
+        tree.insert(new Member1(wiscID, name, yearInSchool, time));
     }
-    public Member1 getMember(long wiscID){
-        //TODO
-        return new Member1(123123, "Must fix", Member1.SchoolYear.valueOf("OTHER"));
+    public Member1 getMember(long wiscID) throws IllegalArgumentException{
+        Member1 memberToReturn = null;
+        //TODO: find member with traversal method
+        if(tree.root == null){
+            throw new IllegalArgumentException("Tree is empty.");
+        }
+        boolean continueSearching = true;
+        RedBlackTree.Node testNode = tree.root;
+        Member1 testMember;
+        while(continueSearching){
+            testMember = (Member1) testNode.data;
+            //This is the correct member
+            if(testMember.wiscID == wiscID){
+                return testMember;
+            }
+            else if(testMember.wiscID < wiscID){
+                if(testNode.rightChild == null){
+                    break;
+                }else{
+                    testNode = testNode.rightChild;
+                }
+            }else if(testMember.wiscID > wiscID){
+                if(testNode.leftChild == null){
+                    break;
+                }else{
+                    testNode = testNode.leftChild;
+                }
+
+            }
+        }
+
+        if(memberToReturn == null)
+            throw new IllegalArgumentException("Member with wiscID of: "+wiscID+" does not exist.");
+        return memberToReturn;
     }
-    //public Member getMember(long iwiscID){
-
-
-      //  return ;
-    //}
 
     /*
     TODO:
         Member getMember(long wiscID)
-        Member removeMember(long wiscID)
         void insertMember(long wiscID, String name, SchoolYear yearInSchool)
         String toString()
         save()
 
      */
+
+    String toReturn = "";
+    @Override
+    public String toString(){
+        toReturn = "";
+        inOrderTraversal(currentMember -> toReturn += currentMember+"\n");
+
+        return toReturn;
+    }
+
+    public static interface Traverser<Member1>{
+        public void visit(Member1 currentMember);
+    }
+    public void inOrderTraversal(Traverser<Member1> t) {
+        this.inOrderTraversal(tree.root, t);
+    }
+    private void inOrderTraversal(RedBlackTree.Node n, Traverser<Member1> t) {
+        if (n != null) {
+            inOrderTraversal(n.leftChild, t);
+            t.visit((Member1) n.data);
+            inOrderTraversal(n.rightChild, t);
+        }
+    }
 
     public void save() {
         //DataUtils.saveData(tree);
