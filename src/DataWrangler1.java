a0ea29345073838b33a6e1e67eaad6891efe62fb
mshen42 <71944458+mshen42@users.noreply.github.com>
2020-10-19 00:56:53 -0500

    Update DataWrangler1.java

--- src/DataWrangler1.java
+++ src/DataWrangler1.java
@@ -1,4 +1,3 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -12,39 +11,47 @@ public class DataWrangler1 {
 	 * @param filePath path of the csv file where the gym data is stored
 	 * @return TreeAndKeys object that stores the red black tree and a list of keys
 	 */
-	public static TreeAndKeys readFile(String filePath) {
-		RedBlackTree<Member> tree = new RedBlackTree<>(); // tree where the data will be stored
+	public static TreeAndKeys readFile() {
+		String fileName = System.getProperty("user.dir");
+		fileName += File.separator + "src" + File.separator + "gym_data.csv";
+
+		RedBlackTree<Member1> tree = new RedBlackTree<>(); // tree where the data will be stored
 		ArrayList<Long> keys = new ArrayList<>(); // array list where the keys will be stored
-		BufferedReader csvReader = null; // reads the csv file
+		Scanner scnr = null; // reads the csv file
+		String csvSplit = ",";
+		String line = "";
+
 		try {
-			// creates a csv reader
-			csvReader = new BufferedReader(new FileReader(filePath));
-			// reads all the data in the file
-			while ((csvReader.readLine() != null)) {
-				String[] data = csvReader.readLine().split(","); // splits the line by comma
+			scnr = new Scanner(new FileReader(fileName)); // allows for data reading
+			scnr.nextLine(); // gets rid of header
+
+			while (scnr.hasNextLine()) {
+				line = scnr.nextLine();
+				String[] data = line.split(csvSplit);
+
+				for (int i = 0; i < data.length; i++) {
+					data[i].strip();
+				}
+
 				long wiscID = Long.parseLong(data[0]); // converts string to long
-				String name = data[1];
-				String year = data[2];
-				Member m = new Member(wiscID, name, year); // creates a new member based on the data
+				String name = data[1]; // name of member
+				String year = data[2].toUpperCase(); // year in school
+
+				Member1 m = new Member1(wiscID, name, Member1.SchoolYear.valueOf(year));
 				tree.insert(m); // inserts the member into the tree
 				keys.add(wiscID); // add the key to the keys array
 			}
 		} catch (Exception e) {
-
+			System.out.println("Error");
+			e.printStackTrace();
 		} finally {
-			if (csvReader != null) {
-				try {
-					csvReader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			if (scnr != null) {
+				scnr.close();
 			}
-
 		}
 
 		// creates a new tree and keys object with the filled tree and keys array
 		TreeAndKeys treeAndKeys = new TreeAndKeys(tree, keys);
-
 		return treeAndKeys;
 	}
 
@@ -55,15 +62,27 @@ public class DataWrangler1 {
 	 * @param gymData tree storing the member objects
 	 * @throws IOException
 	 */
-	public static void write(FileWriter writer, RedBlackTree<Member> gymData) throws IOException {
-		String[] data = gymData.toString().split(",");
+	public static void write(FileWriter writer, RedBlackTree<Member1> gymData) throws IOException {
+		writer.write("wiscID,name,year\n"); // header
+		String treeData = gymData.toString().replace("[", "").replace("]", "");
+		String[] data = treeData.split(",");
 		for (int i = 0; i < data.length; i++) {
+			data[i].strip();
 			writer.write(data[i]);
-			writer.write("\n");
+			if ((i + 1) % 3 == 0) {
+				writer.write("\n");
+				continue;
+			}
+			if (i < data.length) {
+				writer.write(",");
+			}
 		}
 	}
 
-	public static void writeToFile(String fileName, RedBlackTree<Member> gymData) {
+	public static void writeToFile(RedBlackTree<Member1> gymData) {
+		String fileName = System.getProperty("user.dir");
+		fileName += File.separator + "src" + File.separator + "gym_data.csv";
+
 		Scanner sn = new Scanner(System.in);
 		File file = null;
 		FileWriter writer = null;
@@ -74,31 +93,11 @@ public class DataWrangler1 {
 			if (!file.createNewFile()) {
 				fileExists = true;
 			}
+			writer = new FileWriter(file);
+			write(writer, gymData);
 			if (fileExists) {
-				System.out.println(fileName + " already exists.");
-				System.out.println("Do you wish to overwrite " + fileName + " (y/n)?");
-				boolean validInput = false;
-				String userInput = null;
-				while (!validInput) {
-					userInput = sn.nextLine();
-					if (userInput != null) {
-						if (userInput.equals("y") || userInput.equals("n")) {
-							validInput = true;
-						} else {
-							System.out.println("Invalid Input. Enter y or n.");
-						}
-					}
-				}
-				if (userInput != null && userInput.equals("y")) {
-					writer = new FileWriter(file);
-					write(writer, gymData);
-					System.out.println(fileName + " successfully overwritten.");
-				} else {
-					System.out.println(fileName + " was not overwitten.");
-				}
+				System.out.println(fileName + " successfully updated.");
 			} else {
-				writer = new FileWriter(file);
-				write(writer, gymData);
 				System.out.println(fileName + " successfully created.");
 			}
 		} catch (IOException e) {
