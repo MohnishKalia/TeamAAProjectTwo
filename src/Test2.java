61153811e7e72f398ef9925bd5b641db3d558af6
MohnishKalia <mohnish.kalia@gmail.com>
2020-10-18 23:34:16 -0500

    Changed formatting style

--- src/Test2.java
+++ src/Test2.java
@@ -6,82 +6,71 @@
 // TA: Sophie Stephenson
 // Lecturer: Florian Heimerl
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This class tests the implementation of the State1 class
  */
-public class Test2{
+public class Test2 {
 	/**
-	 * Tests importation of the CSV file and
-	 * checks for correct members
-	 */	
-	@Test	
-	public void testImportCSV(){
-	  State1 expected = null;
-	  expectedMembers(expected);
-	  State1 returned = new State1();
-	  assertEquals(expected.toString(),returned.toString(),"Members do not match expected members");
-	  }
-	}
-	
-	/**
-	 * Tests back end exception throws when adding in
-	 * an incorrect wisc id 
+	 * Tests importation of the CSV file and checks for correct members
 	 */
 	@Test
-	public void testAdditionOfMemberWithInvalidID(){
-	  State1 state = new State1();
-	  assertThrows(IllegalArgumentException.class,()->state.insertMember(221L,"Paul McCartney",Member1.SchoolYear.SENIOR));	  
+	public void testImportCSV() {
+		State1 expected = null;
+		expectedMembers(expected);
+		State1 returned = new State1();
+		assertEquals(expected.toString(), returned.toString(), "Members do not match expected members");
 	}
 
 	/**
-	 * Tests back end insertMember method with valid
-	 * member  
+	 * Tests back end exception throws when adding in an incorrect wisc id
 	 */
 	@Test
-	public void testValidInsertMember(){
-	  State1 state = new State1();
-	  assertDoesNotThrow(()->state.insertMember(123456789L,"Chuck Berry",Member1.SchoolYear.SENIOR));	  
+	public void testAdditionOfMemberWithInvalidID() {
+		State1 state = new State1();
+		assertThrows(IllegalArgumentException.class,
+				() -> state.insertMember(221L, "Paul McCartney", Member1.SchoolYear.SENIOR));
 	}
 
-
-
-
 	/**
-	 * Tests back end exception throws when getting member
-	 * that does not exist in tree 
+	 * Tests back end insertMember method with valid member
 	 */
 	@Test
-	public void testRetrievalOfInvalidMember(){
-	  State1 state = new State1();
-	  assertThorws(IllegalArgumentException.class,()->state.getMember(332215789L)); 
+	public void testValidInsertMember() {
+		State1 state = new State1();
+		assertDoesNotThrow(() -> state.insertMember(123456789L, "Chuck Berry", Member1.SchoolYear.SENIOR));
 	}
 
 	/**
-	 * Tests back end getMember method with valid
-	 * member id 
+	 * Tests back end exception throws when getting member that does not exist in
+	 * tree
 	 */
 	@Test
-	public void testValidGetMember(){
-	  State1 state = new State1();
-	  assertDoesNotThrow(()->state.getMember(854148484L));   
+	public void testRetrievalOfInvalidMember() {
+		State1 state = new State1();
+		assertThrows(IllegalArgumentException.class, () -> state.getMember(332215789L));
 	}
 
-
+	/**
+	 * Tests back end getMember method with valid member id
+	 */
+	@Test
+	public void testValidGetMember() {
+		State1 state = new State1();
+		assertDoesNotThrow(() -> state.getMember(854148484L));
+	}
 
 	/**
-	 * Helper method for inserting expected members
-	 * from CSV file
+	 * Helper method for inserting expected members from CSV file
 	 */
-	private static void expectedMembers(State1 expected){
-	  expected.insertMember(854148484L,"Johnny Appleseed",Member1.SchoolYear.FRESHMAN);
-	  expected.insertMember(953089646L,"Paul Blart",Member1.SchoolYear.JUNIOR);
-	  expected.insertMember(103751606L,"Kristin Yang",Member1.SchoolYear.SOPHOMORE);
-	  expected.insertMember(490885798L,"Brad Hayes",Member1.SchoolYear.FRESHMAN);
-	  expected.insertMember(840986425L,"Cherie Corrigan",Member1.SchoolYear.OTHER);
-	  
+	private static void expectedMembers(State1 expected) {
+		expected.insertMember(854148484L, "Johnny Appleseed", Member1.SchoolYear.FRESHMAN);
+		expected.insertMember(953089646L, "Paul Blart", Member1.SchoolYear.JUNIOR);
+		expected.insertMember(103751606L, "Kristin Yang", Member1.SchoolYear.SOPHOMORE);
+		expected.insertMember(490885798L, "Brad Hayes", Member1.SchoolYear.FRESHMAN);
+		expected.insertMember(840986425L, "Cherie Corrigan", Member1.SchoolYear.OTHER);
 	}
 
 }
