d9e7ad0fbd27fc03661a428e6d2e44d965267c6c
Margaret Shen <mshen42@wisc.edu>
2020-10-19 21:36:16 -0500

    updates

--- src/DataWrangler1.java
+++ src/DataWrangler1.java
@@ -17,37 +17,34 @@ import java.util.Scanner;
 public class DataWrangler1 {
 
 	/**
+	 * Method for reading the members.csv file and stores the information into a red black tree 
+	 * 
 	 * @param filePath path of the csv file where the gym data is stored
-	 * @return TreeAndKeys object that stores the red black tree and a list of keys
+	 * @return RedBlackTree<Member1> returns a red black tree that contains the
+	 *         stored member data
 	 */
 	public static RedBlackTree<Member1> readFile() {
-		String fileName = "members.csv";
+		String fileName = "members.csv"; // path to the csv file where the member data is read from
 
 		RedBlackTree<Member1> tree = new RedBlackTree<>(); // tree where the data will be stored
 		Scanner scnr = null; // reads the csv file
-		String csvSplit = ",";
+		String csvSplit = ","; // character that the string will be split by
 		String line = "";
 
 		try {
 			scnr = new Scanner(new FileReader(fileName)); // allows for data reading
 			scnr.nextLine(); // gets rid of header
-
-			while (scnr.hasNextLine()) {
+			while (scnr.hasNextLine()) { // reads every line in the csv file
 				line = scnr.nextLine();
 				String[] data = line.split(csvSplit);
-
-				// makes sure that any extra whitespace is gotten rid of
 				for (int i = 0; i < data.length; i++) {
-					data[i].strip();
+					data[i].strip(); // makes sure that any extra whitespace is gotten rid of
 				}
-
 				long wiscID = Long.parseLong(data[0]); // converts string to long
 				String name = data[1]; // name of member
 				String year = data[2].toUpperCase(); // year in school
-
-				Member1 m = new Member1(wiscID, name, Member1.SchoolYear.valueOf(year));
+				Member1 m = new Member1(wiscID, name, Member1.SchoolYear.valueOf(year)); // creates new member object
 				tree.insert(m); // inserts the member into the tree
-				keys.add(wiscID); // add the key to the keys array
 			}
 		} catch (Exception e) {
 			System.out.println("Error");
@@ -65,11 +62,11 @@ public class DataWrangler1 {
 	 *
 	 * @param writer  file writer
 	 * @param gymData tree storing the member objects
-	 * @throws IOException
 	 */
-	public static void write(FileWriter writer, RedBlackTree<Member1> gymData) throws IOException {
+	private static void write(FileWriter writer, RedBlackTree<Member1> gymData){
 		writer.write("wiscID,name,year\n"); // header
-		String treeData = gymData.toString().replace("[", "").replace("]", ""); // deletes the brackets at the ends of the string
+		String treeData = gymData.toString().replace("[", "").replace("]", ""); // deletes the brackets at the ends of
+																				// the string
 		String[] data = treeData.split(","); // splits string by comma
 
 		// writes the data to the file
@@ -86,10 +83,15 @@ public class DataWrangler1 {
 		}
 	}
 
+	/**
+	 * Takes a red black tree as a parameter and writes the data into a csv file
+	 * 
+	 * @param gymData tree that contains data to be written into the csv file 
+	 */
 	public static void writeToFile(RedBlackTree<Member1> gymData) {
 		String fileName = "members.csv"; // file name
 
-		// defines several objects (to be initalized later)
+		// defines several objects to be initialized in the following code
 		File file = null;
 		FileWriter writer = null;
 		boolean fileExists = false;
@@ -97,20 +99,18 @@ public class DataWrangler1 {
 		try {
 			file = new File(fileName); // initializes file object
 			if (!file.createNewFile()) {
-				fileExists = true;
+				fileExists = true; // checks if a new file is created 
 			}
 			writer = new FileWriter(file); // initializes the file writer
 			write(writer, gymData); // writes to the file
-
-			// prints out informative message
-			if (fileExists) {
+			
+			if (fileExists) { // prints out informative message
 				System.out.println(fileName + " successfully updated.");
 			} else {
 				System.out.println(fileName + " successfully created.");
 			}
-
 		} catch (IOException e) {
-			System.out.println("Error");
+			System.err.println("Error");
 			e.printStackTrace();
 		} finally {
 			try {
@@ -118,7 +118,7 @@ public class DataWrangler1 {
 					writer.close(); // closes writer
 				}
 			} catch (Exception e) {
-				System.out.println("Error");
+				System.err.println("Error");
 				e.printStackTrace();
 			}
 		}
